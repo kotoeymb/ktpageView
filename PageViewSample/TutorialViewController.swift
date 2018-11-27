9ad0b3c7b687175cab11295cbc4f91894c41f952
member <toewaiaung@owaytrip.com>
2018-11-27 10:56:04 +0630

    pageView Fixed

---
+++ PageViewSample/TutorialViewController.swift
@@ -0,0 +1,60 @@
+//
+//  TutorialViewController.swift
+//  UIPageViewController Post
+//
+//  Created by Jeffrey Burt on 2/3/16.
+//  Copyright © 2016 Seven Even. All rights reserved.
+//
+
+import UIKit
+
+class TutorialViewController: UIViewController {
+
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var containerView: UIView!
+    
+    var tutorialPageViewController: TutorialPageViewController? {
+        didSet {
+            tutorialPageViewController?.tutorialDelegate = self
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pageControl.addTarget(self, action: #selector(TutorialViewController.didChangePageControlValue), for: .valueChanged)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let tutorialPageViewController = segue.destination as? TutorialPageViewController {
+            self.tutorialPageViewController = tutorialPageViewController
+        }
+    }
+
+    @IBAction func didTapPreviusButton(_ sender: Any) {
+        tutorialPageViewController?.scrollToPreviusViewController()
+    }
+    @IBAction func didTapNextButton(_ sender: Any) {
+        tutorialPageViewController?.scrollToNextViewController()
+    }
+    
+    /**
+     Fired when the user taps on the pageControl to change its current page.
+     */
+    @objc func didChangePageControlValue() {
+        tutorialPageViewController?.scrollToViewController(index: pageControl.currentPage)
+    }
+}
+
+extension TutorialViewController: TutorialPageViewControllerDelegate {
+    
+    func tutorialPageViewController(tutorialPageViewController: TutorialPageViewController,
+        didUpdatePageCount count: Int) {
+        pageControl.numberOfPages = count
+    }
+    
+    func tutorialPageViewController(tutorialPageViewController: TutorialPageViewController,
+        didUpdatePageIndex index: Int) {
+        pageControl.currentPage = index
+    }
+    
+}
